50a167253780c9db48c8b615eade06499f1c1046
buchgh <94769685+buchgh@users.noreply.github.com>
2021-11-20 23:19:49 -0300

    Commit

--- bodytissues/settings.py
+++ bodytissues/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1yc6&0mkpa8nyg9u*2i9ccy$2%8=i55uq803$mbrn%ofnb_lze
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','miniatlasmorfo.herokuapp.com/']
 
 
 # Application definition
